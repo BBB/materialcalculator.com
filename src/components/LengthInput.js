4fb0f6a482f21264e331ee47de3b0e94154d8946
Ollie Relph <ollie@relph.me>
2016-08-11 22:03:14 +0100

    feat(form): Add a length input

---
+++ src/components/LengthInput.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+
+import UnitDropdown from 'components/UnitDropdown';
+
+export default class LengthInput extends Component {
+
+  static propTypes = {
+    value: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+  }
+
+  render() {
+    const { value, onChange } = this.props;
+    const { unit, amount } = value;
+    const styles = require('./LengthInput.scss');
+    return (
+      <div className={styles.LengthInput}>
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => onChange({
+            ...value,
+            amount: Number(e.target.value),
+          })}
+        />
+        <UnitDropdown
+          onChange={(val) => onChange({
+            ...value,
+            unit: val,
+          })}
+          value={unit}
+        />
+      </div>
+    );
+  }
+}
