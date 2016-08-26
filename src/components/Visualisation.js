a64422348971ff60c3a2a09204bc6f915c6fffb8
Ollie Relph <ollie@relph.me>
2016-08-26 09:44:51 +0100

    fix(svg): Create a single svg so that the defs don't get recreated on area changes

--- src/components/Visualisation.js
+++ src/components/Visualisation.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react';
 
+const GAP = 15;
+
 const Visualisation = (props) => {
   const { areas, maxSize } = props;
   const styles = require('./Visualisation.scss');
@@ -18,67 +20,74 @@ const Visualisation = (props) => {
   }
   return (
     <div>
-      {areas.map((area, ix) => (
-        <svg key={ix} viewBox={`0 0 ${areas[0].w * SCALE_FACTOR} ${areas[0].h * SCALE_FACTOR}`}>
-          <defs>
-            <filter id="fuzzyStroke" height="2" width="2">
-              <feTurbulence baseFrequency="0.4" numOctaves="8" type="fractalNoise" />
-              <feDisplacementMap scale="2" xChannelSelector="R" in="SourceGraphic" />
-            </filter>
-            <pattern id="diagonalHatch" width="10" height="10" patternTransform="rotate(42 0 0)" patternUnits="userSpaceOnUse">
-              <line x1="0" y1="0" x2="0" y2="10" style={{ stroke: 'rgba(255,255,255, 0.2)', strokeWidth: 3 }} />
-            </pattern>
-            <radialGradient id="radialGradient" cx=".5" cy=".5" r=".5">
-              <stop offset="0.5" stopColor="white" stopOpacity="1"/>
-              <stop offset="1" stopColor="white" stopOpacity="0"/>
-            </radialGradient>
-            <mask id="fade" maskContentUnits="objectBoundingBox">
-              <rect width="1" height="1" fill="url(#radialGradient)"/>
-            </mask>
-          </defs>
-          <rect
-            className={styles.Area}
-            x="0"
-            y="0"
-            width={area.w * SCALE_FACTOR}
-            height={area.h * SCALE_FACTOR}
-            style={{ filter: 'url(#fuzzyStroke)'}}
+      <svg viewBox={`0 0 ${areas[0].w * SCALE_FACTOR} ${(areas[0].h * SCALE_FACTOR + GAP) * areas.length}`}>
+        <defs>
+          <filter id="fuzzyStroke" height="2" width="2">
+            <feTurbulence baseFrequency="0.4" numOctaves="8" type="fractalNoise" />
+            <feDisplacementMap scale="2" xChannelSelector="R" in="SourceGraphic" />
+          </filter>
+          <pattern id="diagonalHatch" width="10" height="10" patternTransform="rotate(42 0 0)" patternUnits="userSpaceOnUse">
+            <line x1="0" y1="0" x2="0" y2="10" style={{ stroke: 'rgba(255,255,255, 0.2)', strokeWidth: 3 }} />
+          </pattern>
+          <radialGradient id="radialGradient" cx=".5" cy=".5" r=".5">
+            <stop offset="0.5" stopColor="white" stopOpacity="1"/>
+            <stop offset="1" stopColor="white" stopOpacity="0"/>
+          </radialGradient>
+          <mask id="fade" maskContentUnits="objectBoundingBox">
+            <rect width="1" height="1" fill="url(#radialGradient)"/>
+          </mask>
+        </defs>
+        {areas.map((area, ix) => (
+          <g
+            key={ix}
+            transform={`translate(0, ${(areas[0].h * SCALE_FACTOR + GAP) * ix})`}
           >
-          </rect>
-          {area.blocks.map((block, ix2) => {
-            return (
-              <g
-                key={ix2}
-              >
-                <rect
-                  className={styles.CutBorder + ''}
-                  y={block.fit.y * SCALE_FACTOR}
-                  x={block.fit.x * SCALE_FACTOR}
-                  width={block.w * SCALE_FACTOR}
-                  height={block.h * SCALE_FACTOR}
-                  style={{
-                    filter: 'url(#fuzzyStroke)',
-                  }}
+            <rect
+              className={styles.Area}
+              x="0"
+              y="0"
+              width={area.w * SCALE_FACTOR}
+              height={area.h * SCALE_FACTOR}
+              style={{
+                filter: 'url(#fuzzyStroke)'
+              }}
+            >
+            </rect>
+            {area.blocks.map((block, ix2) => {
+              return (
+                <g
+                  key={ix2}
                 >
-                </rect>
-                <rect
-                  className={styles.CutFiller + ''}
-                  y={block.fit.y * SCALE_FACTOR}
-                  x={block.fit.x * SCALE_FACTOR}
-                  width={block.w * SCALE_FACTOR}
-                  height={block.h * SCALE_FACTOR}
-                  transform={`rotate(${(ix / 10) * 30} ${block.fit.x + (block.w / 2)} ${block.fit.y + (block.h / 2)})`}
-                  style={{
-                    fill: 'url(#diagonalHatch)',
-                    mask: 'url(#fade)',
-                  }}
-                >
-                </rect>
-              </g>
-            );
-          })}
-        </svg>
-      ))}
+                  <rect
+                    className={styles.CutBorder + ''}
+                    y={block.fit.y * SCALE_FACTOR}
+                    x={block.fit.x * SCALE_FACTOR}
+                    width={block.w * SCALE_FACTOR}
+                    height={block.h * SCALE_FACTOR}
+                    style={{
+                      filter: 'url(#fuzzyStroke)',
+                    }}
+                  >
+                  </rect>
+                  <rect
+                    className={styles.CutFiller + ''}
+                    y={block.fit.y * SCALE_FACTOR}
+                    x={block.fit.x * SCALE_FACTOR}
+                    width={block.w * SCALE_FACTOR}
+                    height={block.h * SCALE_FACTOR}
+                    transform={`rotate(${(ix / 10) * 30} ${block.fit.x + (block.w / 2)} ${block.fit.y + (block.h / 2)})`}
+                    style={{
+                      fill: 'url(#diagonalHatch)',
+                      mask: 'url(#fade)',
+                    }}
+                  >
+                  </rect>
+                </g>
+              );
+            })}
+          </g>
+        ))}
+      </svg>
     </div>
   );
 };
